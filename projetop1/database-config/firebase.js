83a4c4dacd0b9f552118d078b58f687bbc099f69
Pedro Lima <pedro.lima@ccc.ufcg.edu.br>
2023-08-30 20:33:54 -0300

    Removing analytics

--- projetop1/database-config/firebase.js
+++ projetop1/database-config/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getFirestore} from "firebase/firestore";
 import {initializeFirestore, CACHE_SIZE_UNLIMITED} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -19,7 +19,7 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 const db = getFirestore();
 
-export {app, analytics, db};
+export {app, db};
